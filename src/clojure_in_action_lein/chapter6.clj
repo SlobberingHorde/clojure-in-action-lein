028b54bf45a371f230e1dc0984a21d1684c70d14
jerryhoerig <jerryhoerig@gmail.com>
2015-01-06 22:50:08 -0500

    Example of futures in Clojure

--- src/clojure_in_action_lein/chapter6.clj
+++ src/clojure_in_action_lein/chapter6.clj
@@ -59,3 +59,27 @@
 
 ;(swap! adi inc) ; causes the watch to fire
 ;(remove-watch adi :adi-watcher) ; removes the watch from the mutable object
+
+
+;;futures in Clojure
+(defn long-calculation [num1 num2]
+  (java.lang.Thread/sleep 5000)
+  (* num1 num2))
+
+(defn long-run []
+  (let [x (long-calculation 11 13)
+        y (long-calculation 13 17)
+        z (long-calculation 17 19)]
+    (* x y z)))
+
+(time (long-run))
+ 
+(defn fast-run []
+  (let [x (future (long-calculation 11 13))
+        y (future (long-calculation 13 17))
+        z (future (long-calculation 17 19))]
+    (* @x @y @z)))
+
+(time (fast-run))
+
+    
