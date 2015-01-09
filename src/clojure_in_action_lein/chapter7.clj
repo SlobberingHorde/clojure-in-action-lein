6596098ebd21b5da1e96ee68f77507e3d9d0bc08
jerryhoerig <jerryhoerig@gmail.com>
2015-01-08 22:47:13 -0500

    Minor update to chapter7 on macros

--- src/clojure_in_action_lein/chapter7.clj
+++ src/clojure_in_action_lein/chapter7.clj
@@ -20,3 +20,24 @@
 ;; shows that the symbols are expanded into place, not evaluated as a function, and THEN the function executes
 (macroexpand 
   '(unless (even? x) (prinln "Very odd indeed")))
+
+(defmacro unless [test then]
+  `(if (not ~test)
+     ~then))
+
+;; The '`' starts the template and everything after it is verbatim
+;; except things that are prefixed with '~'. That unquotes the symbol
+;; expanding it like a variable. ~test and ~then are replaced with their
+;; values.
+
+;; This one allows multiple s-expressions if the test is true
+(defmacro unless [test & exprs]
+  `(if (not ~test)
+     (do ~exprs)))
+;; Fails because the & gathers all of them into a list itself so extra parens are there
+
+;;This splices the contents of the list instead of just unquoting the list
+(defmacro unless [test & exprs]
+  `(if (not ~test)
+     (do ~@exprs)))
+
